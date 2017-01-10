af9a95dc2f42b20022b29e41a9bc575a07066ab8
brunonp <bruno@neoprospecta.com>
2017-01-10 14:58:41 -0200

    External css and template

--- src/angular-x-data-table.component.ts
+++ src/angular-x-data-table.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'angular-x-data-table',
-    styleUrls: ['./angular-x-data-table.component.css'],
+    styleUrls: ['./angular-x-data-table.component.scss'],
     templateUrl: './angular-x-data-table.component.html'
 })
 export class AngularXDataTableComponent {
